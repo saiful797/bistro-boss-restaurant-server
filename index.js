370cf968906265cbb16600f7e05939c60bd0974e
saiful797 <saifulislamsaif797@gmail.com>
2024-05-22 23:27:11 +0600

    Updated user post api

--- index.js
+++ index.js
@@ -36,6 +36,11 @@ async function run() {
     // User related API
     app.post('/users', async(req, res) => {
         const user = req.body;
+        const query = { email: user.email };
+        const existingUser = await userCollection.findOne(query);
+        if(existingUser){
+            return res.send({message: 'User already exist', insertedId: null})
+        }
         const result = await userCollection.insertOne(user);
         res.send(result);
     })
